37c86d5c68849faaec8c3fa9e9a61b19da1775e6
Pedro Leal <pedro.leal.miranda@gmail.com>
2023-05-31 17:02:21 +0100

    react-vite-profile

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,18 @@
+import styles from  "./styles.module.css"
+
+export default function Profile (props){
+    return (
+        <div className={styles.container}>
+           <img className= {styles.avatar} src={props.avatar} alt={props.name} /> 
+           <h2> {props.name}</h2>
+           <div>{props.bio}</div>
+           <div>{props.phone}</div>
+           <div>{props.email}</div>
+           <div className={styles.links}>
+            <a href={props.githubUrl} target="_blank">GitHub</a>
+            <a href={props.linkedinUrl} target="_blank">LinkedIn</a>
+            <a href={props.twitterUrl} target="_blank">Twiter</a>
+           </div>
+        </div>
+    )
+}
